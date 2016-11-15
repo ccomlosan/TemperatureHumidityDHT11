2407b512778853b14ee9fd6ae68c537d5a75f473
ccomlosan <comlosan@yahoo.com>
2016-11-15 16:33:36 +0200

    Add project files.

---
+++ TemperatureHumidityDHT11/DHT11Sensor.cs
@@ -0,0 +1,254 @@
+﻿
+using Windows.Devices.Gpio;
+
+namespace TemperatureHumidityDHT11
+{
+    class DHT11Sensor
+    {
+        enum Error
+        {
+            Sucess = 0,
+            StartSignalLowNotSet = 1,
+            ReadValueTimeout = 2,
+            ReadValueNotDetected = 3
+
+        };
+        enum Context
+        {
+            Null = 0,
+            SendStartSignal = 1,
+            WaitForAckReadLow = 2,
+            WaitForAckReadHigh = 3,
+            ReadBitsReadLow = 4,
+            ReadBitsReadHigh = 5,
+
+        };
+
+        public DHT11Sensor(GpioPin pin, PrecisionCronometer c)
+        {
+            pin_ = pin;
+            cronometer_ = c;
+            cronometer_.start();
+        }
+
+        public void initialize()
+        {
+            cronometer_.start();
+        }
+
+        private void reset()
+        {
+            for (int i = 0; i < SIZE; i++)
+            {
+                data_[i] = 0;
+            }
+            error_ = Error.Sucess;
+            context_ = Context.Null;
+        }
+
+        public void getData(int []data)
+        {
+            if (data.Length >= SIZE - 1)
+            {
+                for(int i = 0; i < SIZE - 2; i++)
+                {
+                    data[i] = data_[i];
+                }
+            }
+        }
+
+        public float measureTicksPerRead(GpioPin testPin, int n)
+        {
+            testPin.SetDriveMode(GpioPinDriveMode.Input);
+            long t1 = cronometer_.ticks;
+            for (int i = 1; i < n; i++)
+            {
+                GpioPinValue val =  testPin.Read();
+            }
+            long t2 = cronometer_.ticks;
+            ticksPerRead_ = t2 - t1;
+            usPerRead_ = cronometer_.ticksToUs(ticksPerRead_)/n;
+            return usPerRead_;
+        }
+
+        public float measureUsPerWrite(GpioPin testPin, int n)
+        {
+            testPin.SetDriveMode(GpioPinDriveMode.Output);
+            long t1 = cronometer_.ticks;
+            for (int i = 1; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    testPin.Write(GpioPinValue.Low);
+                }
+                else
+                {
+                    testPin.Write(GpioPinValue.High);
+                }
+            }
+            long t2 = cronometer_.ticks;
+            ticksPerWrite_ = t2 - t1;
+            usPerWrite_ = cronometer_.ticksToUs(ticksPerWrite_)/n;
+            return usPerWrite_;
+        }
+
+
+        private Error sendStartSignal()
+        {
+            pin_.SetDriveMode(GpioPinDriveMode.Output);
+
+            pin_.Write(GpioPinValue.Low);
+            cronometer_.wait(18000);//-usPerWrite_
+
+            pin_.Write(GpioPinValue.High);
+            cronometer_.wait(30);//-usPerWrite_
+
+            pin_.SetDriveMode(GpioPinDriveMode.Input);
+
+            int count = 0;
+            while(count++<100)
+            {
+                if (pin_.Read() == GpioPinValue.Low)
+                {
+                    return Error.Sucess;
+                }
+            }
+            context_ = Context.SendStartSignal;
+            error_ = Error.StartSignalLowNotSet;
+            return error_;
+        }
+
+        private Error waitForAcknowledge()
+        {
+            long dt;
+
+            if (readValue(GpioPinValue.Low, 100, out dt) == Error.Sucess) {
+                if (readValue(GpioPinValue.High, 100, out dt) == Error.Sucess) {
+                    return Error.Sucess;
+                }
+                else {
+                    context_ = Context.WaitForAckReadHigh;
+                }
+            }
+            else {
+                context_ = Context.WaitForAckReadLow;
+            }
+
+            return error_;
+        }
+
+        private Error readBits()
+        {
+            long dt;
+            int j = 0;
+            for (int i = 1; i <= 40; i++)
+            {
+                if (readValue(GpioPinValue.Low, 70, out dt) == Error.Sucess)
+                {
+                    j = (i - 1) % 8;
+                    data_[j] <<= 1;
+                    if (readValue(GpioPinValue.High, 90, out dt) == Error.Sucess)
+                    {
+                        if (dt >= 60) {
+                            data_[j] |= 0x1;
+                        }
+                    }
+                    else
+                    {
+                        context_ = Context.ReadBitsReadHigh;
+                        return error_;
+                    }
+                }
+                else
+                {
+                    context_ = Context.ReadBitsReadLow;
+                    return error_;
+                }
+                
+            }
+
+            return Error.Sucess;
+        }
+
+
+        public bool read()
+        {
+            reset();
+            if(sendStartSignal() == Error.Sucess)
+            {
+                if (waitForAcknowledge() == Error.Sucess)
+                {
+                    return readBits() == Error.Sucess && checkData() == true;
+
+                }
+            }
+            return false;
+        }
+
+        public bool checkData()
+        {
+            return data_[4] == ((data_[3] + data_[2] + data_[1] + data_[0]) & 0xFF);
+        }
+
+        private Error readValue(GpioPinValue val, long us, out long dt)
+        {
+            GpioPinValue lastReadValue;
+            int readsNr = 0;
+            long tend = cronometer_.getTickToWaitFor(us);
+            long tstart = cronometer_.ticks;
+            do
+            {
+                lastReadValue = pin_.Read();
+                if (lastReadValue == val)
+                {
+                    readsNr++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            while (cronometer_.ticks < tend);
+
+            dt = cronometer_.ticksToUs(cronometer_.ticks - tstart);
+
+            if (lastReadValue == val)
+            {
+                error_ = Error.ReadValueTimeout;
+                return error_;
+            }
+            else
+            {
+                if (readsNr == 0)
+                {
+                    error_ = Error.ReadValueNotDetected;
+                    return error_;
+                }
+                return Error.Sucess;
+            }
+        }
+
+        public string getErrorString()
+        {
+            return "Error=" + errorStrings[(int)error_] + ", context=" + contextStrings[(int)context_];
+        }
+
+        private const int SIZE = 5;
+        private GpioPin pin_;
+        //private GpioPin testPin_;
+        private long ticksPerWrite_ = 0;
+        private float usPerWrite_ = 0;
+        private long ticksPerRead_ = 0;
+        private float usPerRead_ = 0;
+        private PrecisionCronometer cronometer_;
+        private int[] data_ = new int[SIZE];
+
+        private Error error_ = Error.Sucess;
+        private Context context_ = Context.Null;
+
+        static private string []errorStrings = { "Sucess(0)", "StartSignalLowNotSet(1)",
+            "ReadValueTimeout(2)", "ReadValueNotDetected(3)"};
+        static private string []contextStrings = { "Null(0)", "SendStartSignal(1)", "WaitForAckReadLow(2)",
+             "WaitForAckReadHigh(3)", "ReadBitsReadLow(4)", "ReadBitsReadHigh(5)"};
+    }
+}
---
+++ TemperatureHumidityDHT11/MainPage.xaml.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using System.Diagnostics;
+
+using Windows.Devices.Gpio;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TemperatureHumidityDHT11
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            initializeGpio();
+            testClock();
+            if (dht11Pin_ != null)
+            {
+                initializeSensor();
+                //initializeTimer();
+            }
+        }
+
+        private void initializeSensor()
+        {
+            sensor_ = new DHT11Sensor(dht11Pin_, cronometer_);
+            sensor_.initialize();
+            float tpr = sensor_.measureTicksPerRead(testPin_, 100);
+            float tpw = sensor_.measureUsPerWrite(testPin_, 100);
+
+            textBlockPins.Text = string.Format("Pin read op {0}us, write op {1}us", tpr, tpw);
+        }
+
+        //private void initializeTimer()
+        //{
+        //    timer_ = new DispatcherTimer();
+        //    timer_.Interval = TimeSpan.FromSeconds(5);
+        //    timer_.Tick += timerTick;
+        //    timer_.Start();
+        //}
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private bool initializeGpio()
+        {
+            var gpio = GpioController.GetDefault();
+
+            if (gpio == null)
+            {
+                textBlockStatus.Text = "Gpio initialization error";
+                return false;
+            }
+
+            testPin_ = gpio.OpenPin(27);
+            testPin_.SetDriveMode(GpioPinDriveMode.Output);
+            dht11Pin_ = gpio.OpenPin(5);
+            dht11Pin_.SetDriveMode(GpioPinDriveMode.Input);
+
+            textBlockStatus.Text = "Gpio initialized!";
+            return true;
+        }
+
+
+        private void testClock()
+        {
+            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+
+            double d = 123.4567;
+
+            long frequency = Stopwatch.Frequency;
+            long nsPerTick = 1000000000 / frequency;
+            long ticksPerUs = frequency / 1000000;
+            long ticksPerMs = frequency / 1000;
+
+            watch.Start();
+
+            long t1 = watch.ElapsedTicks;
+            d = d / 3.33333;
+            long t2 = watch.ElapsedTicks;
+
+            textBlockTimer.Text = string.Format("isHighPrec:{0}, ticks/sec={1:N}, ns/tick={2:N}, ticks/us={3:N}, ticks/ms={4:N}, ticks/div op={5:N}, div={6:N}",
+                    Stopwatch.IsHighResolution, frequency, nsPerTick, ticksPerUs, ticksPerMs, t2 - t1, d);
+        }
+
+        //private void timerTick(object sender, object args)
+        //{
+        //    textBlockTemperatureHumidity.Text = "Start reading the temperature";
+        //    String displayStr = "";
+        //    int[] data = new int[4];
+        //    PrecisionCronometer c = new PrecisionCronometer();
+        //    c.start();
+        //    long t = c.ticks;
+
+        //    if (sensor_.read())
+        //    {
+        //        sensor_.getData(data);
+        //        displayStr = string.Format("Temperature:{0}.{1}, humidity:{2}.{3} ({4}us)", 
+        //                    data[0], data[1], data[2], data[3], c.ticksToUs(c.ticks - t));
+        //    }
+        //    else
+        //    {
+        //        sensor_.getData(data);
+        //        displayStr = sensor_.getErrorString() + string.Format(", d[0]={0},d[1]={1},d[2]={2},d[3]={3} ({4}us)", 
+        //                    data[0], data[1], data[2], data[3], c.ticksToUs(c.ticks - t));
+        //    }
+        //    textBlockTemperatureHumidity.Text = displayStr;
+        //}
+
+
+        private void buttonTemperatureHumidity_Click(object sender, RoutedEventArgs e)
+        {
+            int[] data = new int[4];
+            PrecisionCronometer c = new PrecisionCronometer();
+            c.start();
+            long t = c.ticks;
+
+            if (sensor_ == null)
+            {
+                textBlockTemperatureHumidity.Foreground = new SolidColorBrush(Windows.UI.Colors.Blue);
+                textBlockTemperatureHumidity.Text = string.Format("Temperature:{0}.{1}, humidity:{2}.{3} ({4}us)",
+                            data[0], data[1], data[2], data[3], c.ticksToUs(c.ticks - t));
+
+                return;
+            }
+
+            if (sensor_.read())
+            {
+                sensor_.getData(data);
+                textBlockTemperatureHumidity.Foreground = new SolidColorBrush(Windows.UI.Colors.Blue);
+                textBlockTemperatureHumidity.Text = string.Format("Temperature:{0}.{1}, humidity:{2}.{3} ({4}us)",
+                            data[0], data[1], data[2], data[3], c.ticksToUs(c.ticks - t));
+            }
+            else
+            {
+                sensor_.getData(data);
+                textBlockTemperatureHumidity.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+                textBlockTemperatureHumidity.Text = sensor_.getErrorString() + string.Format(", d[0]={0},d[1]={1},d[2]={2},d[3]={3} ({4}us)",
+                            data[0], data[1], data[2], data[3], c.ticksToUs(c.ticks - t));
+            }
+        }
+
+
+        private void buttonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Exit();
+        }
+
+
+
+  
+        //private GpioPin testOutPin_;
+        private GpioPin testPin_;
+        private GpioPin dht11Pin_;
+        private DHT11Sensor sensor_;
+        //private DispatcherTimer timer_;
+        private PrecisionCronometer cronometer_ = new PrecisionCronometer();
+
+    }
+}
